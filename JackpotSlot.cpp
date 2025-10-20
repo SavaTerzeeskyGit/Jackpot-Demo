62a3d987e4dd0f88d6b0a0a8245e9bd83f866dc8
Sava Terzeesky <sava.terzeesky@yahoo.com>
2025-10-21 01:05:46 +0300

    # [ADDED] Slot Functionality

--- JackpotSlot.cpp
+++ JackpotSlot.cpp
@@ -1,18 +1,59 @@
 #include "precomp.h"
 #include "JackpotSlot.h"
 
-void SlotEntry::Update(const float& /* deltaTime */)
+JackpotSlot::JackpotSlot(const int2& pos, const int2& widthHeight)
 {
+	SetPos(pos);
+	SetWidthHeight(widthHeight);
 }
 
-void SlotEntry::Draw(const Surface* /* screen */)
+void JackpotSlot::Initialize(const int2& pos, const int2& widthHeight, const int2& spriteWithHeight, const int& padding, const std::array<Sprite, 3>& sprites)
 {
+	SetPos(pos);
+	SetWidthHeight(widthHeight);
+
+	m_Padding = padding;
+	m_Sprites = sprites;
+	m_Borders = widthHeight;
+
+	for (int i = 0; i < m_Entries.size(); i++) {
+		m_Entries[i] = SlotEntry(int2(pos.x + m_PaddingInternal, pos.y + i * m_Padding), spriteWithHeight);
+		m_Entries[i].SetSprite(m_Sprites[i]);
+		m_Entries[i].SetSpeed(1.2f);
+		m_Entries[i].SetBorders(m_Borders);
+	}
 }
 
-void JackpotSlot::Update(const float& /* deltaTime */)
+void JackpotSlot::Update(const float& deltaTime)
 {
+	for (auto& entries : m_Entries) {
+		if (entries.GetHitEdge()) {
+			int randomNumber = rand() % 5;
+			entries.SetSprite(m_TotalSprites[randomNumber]);
+			entries.SetHitEdge(false);
+		}
+		entries.Update(deltaTime);
+	}
 }
 
-void JackpotSlot::Draw(const Surface* /* screen */)
+void JackpotSlot::Draw(const Surface* screen)
 {
+	for (auto& entries : m_Entries) {
+		entries.Draw(screen);
+	}
+}
+
+void JackpotSlot::SetPadding(const int& padding)
+{
+	m_Padding = padding;
+}
+
+void JackpotSlot::SetSprites(const std::array<Sprite, 3>& sprites)
+{
+	m_Sprites = sprites;
+}
+
+void JackpotSlot::SetTotalSprites(const std::array<Sprite, 5>& sprites)
+{
+	m_TotalSprites = sprites;
 }
--- JackpotSlot.h
+++ JackpotSlot.h
@@ -1,18 +1,7 @@
 #pragma once
 #include "Base.h"
-
-class SlotEntry : public Base
-{
-public:
-	SlotEntry() = default;
-	~SlotEntry() override = default;
-
-	void Update(const float& deltaTime) override;
-	void Draw(const Surface* screen) override;
-
-private:
-	// Spite
-};
+#include "SlotEntry.h"
+#include "array"
 
 class JackpotSlot : public Base
 {
@@ -20,9 +9,24 @@ public:
 	JackpotSlot() = default;
 	~JackpotSlot() override = default;
 
+	JackpotSlot(const int2& pos, const int2& widthHeight);
+
+	void Initialize(const int2& pos, const int2& widthHeight, const int2& spriteWithHeight, const int& padding, const std::array<Sprite, 3>& sprites);
+
 	void Update(const float& deltaTime) override;
 	void Draw(const Surface* screen) override;
 
+	void SetPadding(const int& padding);
+	void SetSprites(const std::array<Sprite, 3>& sprites);
+	void SetTotalSprites(const std::array<Sprite, 5>& sprites);
+
+	inline const std::array<SlotEntry, 3>& GetEntries() const { return m_Entries; };
+
 private:
-	//SlotEntry entries[3];
+	std::array<SlotEntry, 3> m_Entries{};
+	std::array<Sprite, 3> m_Sprites{};
+	std::array<Sprite, 5> m_TotalSprites{};
+	int2 m_Borders = int2(0);
+	int m_Padding = 50;
+	int m_PaddingInternal = 50;
 };
